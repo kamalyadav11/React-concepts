3ea023f86515f3edff1b2389c00c9416995230f9
kamal11 <kyzx11@gmail.com>
2018-11-12 14:41:49 +0530

    removed comment

--- src/ConditionalsLists/index.js
+++ src/ConditionalsLists/index.js
@@ -45,22 +45,6 @@ class ConditionalLists extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {/* <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "Kamal")}
-            changed={this.nameChangeHandler}
-          >
-            My Hobbies: Racing
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          /> */}
           {this.state.persons.map((person, index) => (
             <Person
               key={person.name}
