37a1e88daa1e3bad1dc5579049dd16a7a28b16a1
kamal11 <kyzx11@gmail.com>
2018-11-13 22:32:31 +0530

    solved hoc bug

--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -3,7 +3,8 @@ import React from "react";
 const withClass = (WrappedComponent, className) => {
   return props => (
     <div className={className}>
-      <WrappedComponent />
+      <WrappedComponent {...props} />
+      {/**pass props to wrapped component as you get them */}
     </div>
   );
 };
