538db0189a84e95ea779efd94f6d6f329c08d613
kamal11 <kyzx11@gmail.com>
2018-11-13 23:02:06 +0530

    removed PropType typo and added ref

--- src/ConditionalsLists/components/Persons/Person/Person.js
+++ src/ConditionalsLists/components/Persons/Person/Person.js
@@ -6,6 +6,10 @@ import Aux from "../../../../hoc/Aux";
 import withClass from "../../../../hoc/withClass";
 
 class Person extends React.Component {
+  componentDidMount() {
+    this.inputElement.focus();
+  }
+
   render() {
     return (
       <Aux>
@@ -13,13 +17,19 @@ class Person extends React.Component {
           I'm {this.props.name} and I am {this.props.age} years old
         </p>
         <p>{this.props.children}</p>
-        <input onChange={this.props.changed} value={this.props.name} />
+        <input
+          ref={inp => {
+            this.inputElement = inp;
+          }}
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
       </Aux>
     );
   }
 }
 
-Person.prototype = {
+Person.PropsTypes = {
   click: PropTypes.func,
   name: PropTypes.string,
   age: PropTypes.number,
